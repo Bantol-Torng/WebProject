67be5cc6e50fa5a9481c9f14c3aa9e362362be88
Torng Bantol <bantoltorng1@gmail.com>
2026-01-03 12:11:26 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
-let allProjects = [];
-
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('projects-container');
-    const buttons = document.querySelectorAll('.filter-btn');
-
-    fetch('projects.json')
-        .then(response => response.json())
-        .then(data => {
-            allProjects = data;
-            renderProjects('all');
-        })
-        .catch(err => console.error(err));
-
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            buttons.forEach(b => b.classList.remove('active'));
-            btn.classList.add('active');
-            const category = btn.getAttribute('data-filter');
-            renderProjects(category);
-        });
+function moveSlide(direction) {
+    const slider = document.getElementById('projectSlider');
+    const scrollAmount = 295; 
+    slider.scrollBy({
+        left: direction * scrollAmount,
+        behavior: 'smooth'
     });
-
-    function renderProjects(filter) {
-        if (!container) return;
-        container.innerHTML = '';
-        const filteredList = filter === 'all' ? allProjects : allProjects.filter(p => p.category === filter);
-        filteredList.forEach(project => {
-            container.innerHTML += `
-                <div class="project-item">
-                    <img src="${project.image}" alt="${project.title}">
-                    <h3>${project.title}</h3>
-                    <p>${project.description}</p>
-                    <div class="tags">
-                        ${project.tags.map(tag => `<span>${tag}</span>`).join('')}
-                    </div>
-                </div>
-            `;
-        });
+}
+function moveAboutSlide(direction) {
+    const slider = document.getElementById('aboutSlider');
+    const scrollAmount = 295; 
+    slider.scrollBy({
+        left: direction * scrollAmount,
+        behavior: 'smooth'
+    });
+}
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(18, 18, 18, 0.6)";
+    } else {
+        nav.style.background = "transparent";
     }
 });
